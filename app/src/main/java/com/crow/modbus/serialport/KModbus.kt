978cf7157a035fca51fbf3c9d08bccc101787c44
crowforkotlin <linzepeng@listentech.com.cn>
2023-10-16 16:37:45 +0800

    :poop: ◉ 改进KModbus.kt 中的多线圈写入实现

--- app/src/main/java/com/crow/modbus/serialport/KModbus.kt
+++ app/src/main/java/com/crow/modbus/serialport/KModbus.kt
@@ -64,57 +64,6 @@ open class KModbus protected constructor() {
                 //写入数据
                 (values ?: throw ModbusException(ModbusErrorType.ModbusInvalidArgumentError, "Function Is $function, Data must be passed in!")).forEach { output.writeInt16(it) }
             }
-            /*ModbusFunction.WRITE_COILS -> {
-                    if (values == null) throw ModbusException(ModbusErrorType.ModbusInvalidArgumentError, "Function Is $function, Data must be passed in!")
-
-                    //计算写入字节数
-                    var writeByteCount: Int = count / 8  // 8个线圈 代表 一个字节
-
-                    // 如果剩余线圈数量不是8的倍数，则需要再加一个字节
-                    if (count % 8 != 0) {
-                        writeByteCount += 1
-                    }
-
-                    //写多个线圈寄存器
-                    output.writeInt8(function.mCode)
-                    output.writeInt16(startAddress)
-                    output.writeInt16(count)
-                    output.writeInt8(writeByteCount)
-
-                    var index = 0
-                    //如果写入数据数量 > 8 ，则需要拆分开来
-                    var start = 0 //数组开始位置
-
-                    var end = 7 //数组结束位置
-
-                    var splitData = IntArray(8)
-                    //循环写入拆分数组，直到剩下最后一组 元素个数 <= 8 的数据
-                    while (writeByteCount > 1) {
-                        writeByteCount--
-                        var sIndex = 0
-                        index = start
-                        while (index <= end) {
-                            splitData[sIndex++] = values[index]
-                            index++
-                        }
-                        //数据反转 对于是否要反转要看你传过来的数据，如果高低位顺序正确则不用反转
-                        splitData = splitData.reversedArray()
-
-                        //写入拆分数组
-                        output.writeInt8(toDecimal(splitData))
-                        start = index
-                        end += 8
-                    }
-                    //写入最后剩下的数据
-                    val last: Int = count - index
-                    var tData: IntArray? = IntArray(last)
-                    System.arraycopy(values, index, tData, 0, last)
-                    //数据反转 对于是否要反转要看你传过来的数据，如果高低位顺序正确则不用反转
-                    tData = tData!!.reversedArray()
-
-                    output.writeInt8(toDecimal(tData))
-            }*/
-
             ModbusFunction.WRITE_COILS -> {
                 if (values == null || values.isEmpty()) throw ModbusException(ModbusErrorType.ModbusInvalidArgumentError, "Function Is $function, Data must be passed in and cannot be empty!")
                 output.writeInt8(function.mCode)
@@ -126,7 +75,6 @@ open class KModbus protected constructor() {
                     output.writeInt8(toDecimal(chunk.reversed().toIntArray()))
                 }
             }
-            else -> throw ModbusException(ModbusErrorType.ModbusError, "unknown function code!")
         }
 
         return output
