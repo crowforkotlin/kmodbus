b2d9a6c3ecc28d365faa6306cc66bbed8d59cb63
WuYa <crowforkotlin@gmail.com>
2024-01-27 19:33:44 +0800

    :fire: 发布至Maven

---
+++ lib_modbus/obj/local/arm64-v8a/objs/SerialPort/SerialPort.o.d
@@ -0,0 +1,4 @@
+/Users/crowforkotlin/AndroidStudioProjects/KModbus/lib_modbus/obj/local/arm64-v8a/objs/SerialPort/SerialPort.o: \
+  /Users/crowforkotlin/AndroidStudioProjects/KModbus/lib_modbus/jni/SerialPort.c \
+  /Users/crowforkotlin/AndroidStudioProjects/KModbus/lib_modbus/jni/SerialPort.h
+/Users/crowforkotlin/AndroidStudioProjects/KModbus/lib_modbus/jni/SerialPort.h:
---
+++ lib_modbus/obj/local/armeabi-v7a/objs/SerialPort/SerialPort.o.d
@@ -0,0 +1,4 @@
+/Users/crowforkotlin/AndroidStudioProjects/KModbus/lib_modbus/obj/local/armeabi-v7a/objs/SerialPort/SerialPort.o: \
+  /Users/crowforkotlin/AndroidStudioProjects/KModbus/lib_modbus/jni/SerialPort.c \
+  /Users/crowforkotlin/AndroidStudioProjects/KModbus/lib_modbus/jni/SerialPort.h
+/Users/crowforkotlin/AndroidStudioProjects/KModbus/lib_modbus/jni/SerialPort.h:
